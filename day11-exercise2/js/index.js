ce567a95acdcb06fe7e6b5b51150ae579e6d811e
Yusuf Demiroz 00858166 <yusuf.demiroz@huawei.com>
2024-04-29 09:28:13 +0300

    Added day11-exercise1 and day11-exercise2

---
+++ day11-exercise2/js/index.js
@@ -0,0 +1,70 @@
+const constants = [2.72, 3.14, 9.81, 37, 100]
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: 200,
+    perimeter: 60
+}
+const users = [
+    {
+        name: 'Brook',
+        scores: 75,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 16
+    },
+    {
+        name: 'Alex',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'David',
+        scores: 75,
+        skills: ['HTM', 'CSS'],
+        age: 22
+    },
+    {
+        name: 'John',
+        scores: 85,
+        skills: ['HTML'],
+        age: 25
+    },
+    {
+        name: 'Sara',
+        scores: 95,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name: 'Martha',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'Thomas',
+        scores: 90,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+    }
+]
+
+console.log("*****************");
+
+// Iterate through the users array and get all the keys of the object using destructuring
+
+for (let { name, scores, skills, age } of users) {
+    console.log(name, scores, skills, age);
+}
+
+console.log("*****************");
+
+// Find the persons who have less than two skills
+
+for (let { name, scores, skills, age } of users) {
+    if (skills.length < 2) {
+        console.log(name, scores, skills, age);
+    }
+}
